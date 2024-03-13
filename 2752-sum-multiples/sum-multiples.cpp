aba8717b8e489e80a756c73e6a3fe9ea4c85dec6
Yashovardhan <yashovardhan15@gmail.com>
2024-03-13 20:04:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (34.03%) - LeetSync

--- 2752-sum-multiples/sum-multiples.cpp
+++ 2752-sum-multiples/sum-multiples.cpp
@@ -1,10 +1,24 @@
+// class Solution {
+// public:
+//     int sumOfMultiples(int n) {
+//         int sum=0;
+//         for(int i=3; i<=n; i++){
+//             if(i%3 == 0 || i%5 == 0 || i%7 == 0){
+//                 sum += i;
+//             }
+//         }
+//         return sum;
+        
+//     }
+// };
+
 class Solution {
 public:
     int sumOfMultiples(int n) {
         int sum=0;
-        for(int i=3; i<=n; i++){
-            if(i%3 == 0 || i%5 == 0 || i%7 == 0){
-                sum += i;
+        for(int i=3;i<=n;i++){
+            if((i%3)==0||(i%5)==0||(i%7)==0){
+                sum=sum+i;
             }
         }
         return sum;
