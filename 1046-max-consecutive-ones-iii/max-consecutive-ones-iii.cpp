0b64bc321333185c562be5de985fc68c7894f3dc
Yashovardhan <yashovardhan15@gmail.com>
2024-03-20 08:09:42 +0530

    Time: 41 ms (59.56%) | Memory: 57.8 MB (50.32%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -6,9 +6,10 @@ public:
         for(int r=0; r<n; r++){
             if(nums[r] == 0) zero_count++;
             while(zero_count > k){
-                if(nums[l] == 0)
+                if(nums[l] == 0){
                     zero_count--;
-                    l++;
+                }
+                l++;
             }
             ans = max(ans, r-l+1);
         } 
