8e7d6aeb4ee5e31044f8f98de827626f23519c6b
Yashovardhan <yashovardhan15@gmail.com>
2024-04-07 22:44:10 +0530

    Time: 77 ms (97.19%) | Memory: 70.1 MB (72.67%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,41 +1,49 @@
+// class Solution {
+// public:
+//     vector<int> merge2sortedarrays(vector<int> &left, vector<int> &right){
+//         vector<int> ans;
+//         int i=0, j=0;
+//         int m = left.size();
+//         int n = right.size();
+//         while(i<m && j<n){
+//             if(left[i] < right[j]){
+//                 ans.push_back(left[i]);
+//                 i++;
+//             }
+//             else{
+//                 ans.push_back(right[j]);
+//                 j++;
+//             }
+//         }
+//         while(i<m){
+//             ans.push_back(left[i]);
+//             i++;
+//         }
+//         while(j<n){
+//             ans.push_back(right[j]);
+//             j++;
+//         }
+//         return ans;
+//     }
+
+// public:
+//     vector<int> sortArray(vector<int>& nums){
+//         int n = nums.size();
+//         if(n == 1) return nums;
+//         vector<int> left(nums.begin(), nums.begin() + n/2);
+//         vector<int> right(nums.begin() + n/2, nums.end());
+
+//         left = sortArray(left);
+//         right = sortArray(right);
+
+//         return merge2sortedarrays(left, right);
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> merge2sortedarrays(vector<int> &left, vector<int> &right){
-        vector<int> ans;
-        int i=0, j=0;
-        int m = left.size();
-        int n = right.size();
-        while(i<m && j<n){
-            if(left[i] < right[j]){
-                ans.push_back(left[i]);
-                i++;
-            }
-            else{
-                ans.push_back(right[j]);
-                j++;
-            }
-        }
-        while(i<m){
-            ans.push_back(left[i]);
-            i++;
-        }
-        while(j<n){
-            ans.push_back(right[j]);
-            j++;
-        }
-        return ans;
-    }
-
-public:
-    vector<int> sortArray(vector<int>& nums){
-        int n = nums.size();
-        if(n == 1) return nums;
-        vector<int> left(nums.begin(), nums.begin() + n/2);
-        vector<int> right(nums.begin() + n/2, nums.end());
-
-        left = sortArray(left);
-        right = sortArray(right);
-
-        return merge2sortedarrays(left, right);
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
