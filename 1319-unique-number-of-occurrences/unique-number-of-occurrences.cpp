384ba46a7d78025ff67bf1a18f9c51d027b68099
Yashovardhan <yashovardhan15@gmail.com>
2024-04-14 23:16:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (67.84%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,17 +1,40 @@
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map<int, int>count;
+//         unordered_set<int>st;
+
+//         for(const int a:arr){
+//             count[a]++;
+//         }
+//         for(auto i:count){
+//             st.insert(i.second);
+//         }
+//         if(count.size() == st.size()) return true;
+//         return false;
+
+//     }
+// };
+
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int>count;
-        unordered_set<int>st;
-
-        for(const int a:arr){
-            count[a]++;
+        map<int,int>m;
+        for(int i=0;i<arr.size();i++)
+        {
+            m[arr[i]]++;
         }
-        for(auto i:count){
-            st.insert(i.second);
+        vector<int>ans;
+        for(auto i:m)
+        {
+            ans.push_back(i.second);
+            
         }
-        if(count.size() == st.size()) return true;
-        return false;
-
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<ans.size()-1;i++)
+        {
+            if(ans[i]==ans[i+1])return false;
+        }
+        return true;
     }
 };
