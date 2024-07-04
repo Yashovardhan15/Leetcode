6cea7716b74f685016ce02906c461af87847eadb
Yashovardhan <yashovardhan15@gmail.com>
2024-07-04 22:39:43 +0530

    Time: 4 ms (20.32%) | Memory: 7.9 MB (93.42%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -5,7 +5,7 @@ class Solution {
     if (n == 0)
       return 1;
     if (n < 0)
-      return 1 / myPow(x, -n);
+      return 1 / myPow(x, abs(n));
     if (n % 2 == 1)
       return x * myPow(x, n - 1);
     return myPow(x * x, n / 2);
